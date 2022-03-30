57696c1172e81ade900f1c61296308595858a790
yuehingyu <yu.yuehing@gmail.com>
2022-03-29 20:36:29 -0400

    remove keys from app.properties file

--- src/main/java/demo/azure/cosmo/api/CosmosClientFactory.java
+++ src/main/java/demo/azure/cosmo/api/CosmosClientFactory.java
@@ -16,27 +16,19 @@ import com.azure.cosmos.CosmosClientBuilder;
 @Configuration
 public class CosmosClientFactory {
 
-	@Value("${azure.datasource.host}")
+	@Value("#{environment.COSMOS_HOST}")
 	private String HOST;
 
-	@Value("${azure.datasource.master_key}")
-	private String MASTER_KEY;
-
-	@Value("#{environment.COSMOS_HOST}")
-	private String GLOBAL_HOST;
-
 	@Value("#{environment.COSMOS_MASTER_KEY}")
-	private String GLOBAL_MASTER_KEY;
+	private String MASTER_KEY;
 
 	@Bean(name = "myCosmoClient")
 	@Primary
 	public CosmosClient myCosmosClient() {
 
+	
 		System.out.println("HOST is : " + HOST);
 		System.out.println("MASTER KEY is : " + MASTER_KEY);
-
-		System.out.println("GLOBAL_HOST is : " + GLOBAL_HOST);
-		System.out.println("GLOBAL MASTER KEY is : " + GLOBAL_MASTER_KEY);
 
 		CosmosClient cosmosClient = new CosmosClientBuilder().endpoint(HOST).key(MASTER_KEY)
 				.consistencyLevel(ConsistencyLevel.STRONG).buildClient();
